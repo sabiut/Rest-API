98bba54c4d443dbee96a0a8a8f6fb3a858993b60
waikato_sa252 <sa252@waikato.students.ac.nz>
2020-06-21 14:00:49 +1200

    update auth

--- movies/views.py
+++ movies/views.py
@@ -20,7 +20,7 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
