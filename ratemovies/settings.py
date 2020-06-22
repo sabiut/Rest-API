50322d64b1e17d18477794eaf24b07651f5a4b47
waikato_sa252 <sa252@waikato.students.ac.nz>
2020-06-23 02:16:42 +1200

    update auth

--- ratemovies/settings.py
+++ ratemovies/settings.py
@@ -51,13 +51,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-#CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = [
-
-    "http://localhost:3000",
-
-]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = [
+#
+#     "http://localhost:3000",
+#
+# ]
 
 ROOT_URLCONF = 'ratemovies.urls'
 
