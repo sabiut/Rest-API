345172c0f0655df7c59f1f82e48a851f78161a90
waikato_sa252 <sa252@waikato.students.ac.nz>
2020-06-22 23:24:52 +1200

    update db

--- ratemovies/settings.py
+++ ratemovies/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL=True
+#CORS_ORIGIN_ALLOW_ALL=True
 # CORS_ORIGIN_WHITELIST = [
 #     "https://example.com",
 #     "https://sub.example.com",
